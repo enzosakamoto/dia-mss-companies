3b3c698e1383238cc6ca7bc2a6800a359467256d
Enzo Sakamoto <enzo.sak@hotmail.com>
2023-08-15 11:17:18 -0300

    feat: ensure GetCompaniesMongoRepository gets companies from mongodb

--- src/repositories/get_companies/mongo_get_companies.ts
+++ src/repositories/get_companies/mongo_get_companies.ts
@@ -1,16 +1,17 @@
 import { IGetCompaniesRepository } from '../../controllers/get_companies/protocols'
+import { MongoClient } from '../../database/mongo'
 import { Company } from '../../models/company'
 
 export class MongoGetCompaniesRepository implements IGetCompaniesRepository {
   async getCompanies(): Promise<Company[]> {
-    return [
-      {
-        id: 1,
-        name: 'Company 1',
-        image: 'asd',
-        description: 'asd',
-        link: 'asd'
-      }
-    ]
+    const companies = await MongoClient.db
+      .collection<Omit<Company, 'id'>>('companies')
+      .find({})
+      .toArray()
+
+    return companies.map(({ _id, ...rest }) => ({
+      id: _id.toHexString(),
+      ...rest
+    }))
   }
 }
