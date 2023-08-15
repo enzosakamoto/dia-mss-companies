3a5773af953582d7c628cf75bc917cd46fddb680
Enzo Sakamoto <enzo.sak@hotmail.com>
2023-08-15 09:58:15 -0300

    chore: add express setup

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-const a = 'udhaushduhsaudhsa'
+import express from 'express'
+import { config } from 'dotenv'
+
+config()
+
+const app = express()
+
+const port = process.env.PORT || 3000
+
+app.get('/', (req, res) => {
+  res.send('API is running! 🚀')
+})
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`)
+})
