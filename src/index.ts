9b84c33991fe56ff3a67124224c61654b555519a
Enzo Sakamoto <enzo.sak@hotmail.com>
2023-08-15 11:03:34 -0300

    feat: add MongoClient with MongoDB Atlas connection

--- src/index.ts
+++ src/index.ts
@@ -2,28 +2,34 @@ import express from 'express'
 import { config } from 'dotenv'
 import { GetCompaniesController } from './controllers/get_companies/get_companies'
 import { MongoGetCompaniesRepository } from './repositories/get_companies/mongo_get_companies'
+import { MongoClient } from './database/mongo'
 
-config()
+const main = async () => {
+  config()
+  const app = express()
 
-const app = express()
+  await MongoClient.connect()
 
-const port = process.env.PORT || 3000
+  const port = process.env.PORT || 3000
 
-app.get('/', (req, res) => {
-  res.send('API is running! 🚀')
-})
+  app.get('/', (req, res) => {
+    res.send('API is running! 🚀')
+  })
 
-app.get('/companies', async (req, res) => {
-  const mongoGetUsersRepository = new MongoGetCompaniesRepository()
-  const getCompaniesController = new GetCompaniesController(
-    mongoGetUsersRepository
-  )
+  app.get('/companies', async (req, res) => {
+    const mongoGetUsersRepository = new MongoGetCompaniesRepository()
+    const getCompaniesController = new GetCompaniesController(
+      mongoGetUsersRepository
+    )
 
-  const response = await getCompaniesController.handle()
+    const response = await getCompaniesController.handle()
 
-  res.status(response.statusCode).json(response.body)
-})
+    res.status(response.statusCode).json(response.body)
+  })
 
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`)
-})
+  app.listen(port, () => {
+    console.log(`Server running on port ${port}`)
+  })
+}
+
+main()
