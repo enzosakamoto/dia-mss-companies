2425d9deaa7298708de418d892ceeb9e5e1c2509
Enzo Sakamoto <enzo.sak@hotmail.com>
2023-08-16 15:05:49 -0300

    add: http status code enum

--- src/controllers/helpers.ts
+++ src/controllers/helpers.ts
@@ -1,25 +1,25 @@
-import { HttpResponse } from './protocols'
+import { HTTP_STATUS_CODE, HttpResponse } from './protocols'
 
 export const ok = <T>(body: any): HttpResponse<T> => ({
-  statusCode: 200,
+  statusCode: HTTP_STATUS_CODE.OK,
   body
 })
 
 export const created = <T>(body: any): HttpResponse<T> => ({
-  statusCode: 201,
+  statusCode: HTTP_STATUS_CODE.CREATED,
   body
 })
 
 export const badRequest = (message: string): HttpResponse<string> => {
   return {
-    statusCode: 400,
+    statusCode: HTTP_STATUS_CODE.BAD_REQUEST,
     body: message
   }
 }
 
 export const internalServerError = (message: string): HttpResponse<string> => {
   return {
-    statusCode: 500,
+    statusCode: HTTP_STATUS_CODE.INTERNAL_SERVER_ERROR,
     body: message
   }
 }
