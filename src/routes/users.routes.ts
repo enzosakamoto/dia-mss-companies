f3d5d7eb57541f695ab3dc81cb4ecc722ad77552
Enzo Sakamoto <enzo.sak@hotmail.com>
2023-08-29 17:46:57 -0300

    feat: add token validation

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,3 +1,4 @@
+import jwt from 'jsonwebtoken'
 import express from 'express'
 import { MongoGetLoginRepository } from '../repositories/get_login/mongo_get_login'
 import { GetLoginController } from '../controllers/get_login/get_login'
@@ -12,6 +13,28 @@ userRouter.post('/', async (req, res) => {
   })
 
   res.status(statusCode).send(body)
+})
+
+userRouter.post('/validate', (req, res) => {
+  const { token } = req.body
+
+  if (!token) {
+    return res.status(401).send({ message: 'No token provided.' })
+  }
+
+  try {
+    const secret = process.env.SECRET
+
+    if (!secret) {
+      throw new Error('Something went wrong')
+    }
+
+    jwt.verify(token, secret)
+
+    res.status(200).send({ isValid: true, message: 'Token is valid' })
+  } catch (error) {
+    res.status(401).send({ isValid: false, message: 'Token is not valid' })
+  }
 })
 
 export default userRouter
